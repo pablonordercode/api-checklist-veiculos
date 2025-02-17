22ed5a5cb3cba65e16c06435b5b405984aea0247
Pablo romulo <112846833+pablonordercode@users.noreply.github.com>
2025-02-16 23:05:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const conectarDataBase = require("./db/conn");
 const userRoutes = require("./routes/userRoutes");
 const checkListModels = require("./routes/checkListRoutes");
 const motoristaControlers = require("./routes/motoristaRoutes");
 const vigiaRotes = require("./routes/vigiaRoutes");
-const conectarDataBase = require("./db/conn");
-
-const cors = require('cors');
-app.use(cors({
-  origin: '*', // Permite requisições de qualquer origem (em produção, use domínios específicos)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos permitidos
-}));
-
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 7001;
 
-conectarDataBase()
+// Conexão com o banco de dados
+conectarDataBase();
+
+// Middlewares
 app.use(express.json());
-// Servir arquivos estáticos da pasta uploads
-app.use('/uploads', express.static('uploads'))
 app.use(cors());
 
+// Servir arquivos estáticos da pasta uploads
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-
+// Rotas
 app.use("/usuario", userRoutes);
 app.use("/checklist", checkListModels);
-app.use("/motorista", motoristaControlers); 
-app.use("/vigia", vigiaRotes); 
+app.use("/motorista", motoristaControlers);
+app.use("/vigia", vigiaRotes);
 
+// Inicializar o servidor
 app.listen(PORT, () => {
-    console.log(`rodando na port: ${PORT}`);
+  console.log(`Servidor rodando na porta: ${PORT}`);
 });
+
+
